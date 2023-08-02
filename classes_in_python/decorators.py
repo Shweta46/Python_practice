4f51f5c57d54b49dce5e45f2986e44ee3e743761
shweta <aryashweta46@gmail.com>
2023-08-02 16:59:50 +0530

    classes

---
+++ classes_in_python/decorators.py
@@ -0,0 +1,93 @@
+
+# 1. Treating functions as objects:
+
+def shout(text):
+    return text.upper()
+print(shout('Hello'))
+yell = shout
+print(yell('Hello'))
+
+# In the above example, we have assigned the function shout to a variable.
+# This will not call the function instead it takes the function object referenced by a shout and creates a second name pointing to it, yell.
+
+# 2. Passing the function as an argument
+def shout(text):
+    return text.upper()
+def whisper(text):
+    print("Lower text")
+    return text.lower()
+def greet(func):
+    greeting = func('Hi, I am created by a function passed as an argument.')
+    print(greeting)
+
+greet(shout)
+greet(whisper)
+
+# 3. RETURNING FUNCTIONS FROM ANOTHER FUNCTION:
+def create_adder(x):
+    def adder(y):
+        # print(x)
+        return x+y
+    return adder
+# add_15 = create_adder(15)(10)
+# print(add_15)
+# The functon create_adder, when run, returns a function adder. This returned function can be interpreted as:
+# add_15 = adder(y)
+# Now, we assign the argument to add_15 as:
+add_15 = create_adder(15)
+print(add_15(10))
+
+# THE ABOVE CODING LINES CAN ALSO BE WRITTEN AS:
+# add_15 = create_adder(x)(y)
+# print(add_15)
+
+# **********************************************************************************************************************************************
+# DECORATORS:
+
+def hello_decorator(func):
+    print('Hello, this is before function execution.')
+    def inner1():
+        print('Hello, this is before function execution.')
+        func()
+        print('This is after function execution.')
+    return inner1
+def function_to_be_used():
+    print('This is inside the function.')
+
+function_to_be_used = hello_decorator(function_to_be_used)
+function_to_be_used()
+
+# THE ABOVE CODE LINES ARE TO BE INTERPRETED AS:
+
+# function_to_be_used() = hello_decorator(function_to_be_used)
+# this makes func = function_to_be_used()
+# then, hello_decorator() will return the inner1 function.
+# This makes:
+# function_to_be_used = inner1()
+# So, when finally function_to_be_used is called, its like calling the inner1 function.
+# This triggers the function inside the hello_decorator and returns the print statement of inner1 and then func (which became equal to function_to_be_used) then last after execution print statement.
+
+#
+# def add(f):
+#     def inner():
+#         return f() + 64
+#     return inner
+# def foo():
+#     return 5
+# foo = add(foo)
+#
+# print(foo())
+# THE USE OF KEYWORD "@_DECORATOR"
+def add(f):
+    def inner():
+        print('hi')
+        return f() + 64
+    return inner
+@add
+def foo():
+    return 5
+
+# THE CODE LINES @add
+# def foo() translates to: foo = add(foo)
+
+print(foo())
---
+++ linked_list_operations/sort_the_linked_list.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def bub_sort(self, list):
+        n = len(list)
+        for i in range(n):
+            min = list[i]
+            j = i - 1
+            while j >= 0 and min < list[j]:
+                list[j + 1] = list[j]
+                j -= 1
+            list[j+1] = min
+        return list
+
+
+    def sort_list(self):
+        temp = self.head
+        l = []
+        while temp is not None:
+            l.append(temp.data)
+            temp = temp.next
+        print(l)
+        self.bub_sort(l)
+        print('Sorted list: ')
+        print(l)
+        self.head = l[0]
+        for i in range(1, len(l) - 1):
+            self.push(l[i])
+        print(l)
+        return l
+
+
+    def printlist(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+llist = linkedlist()
+llist.push(11)
+llist.push(1)
+llist.push(10)
+llist.push(9)
+llist.push(7)
+llist.push(0)
+llist.push(2)
+llist.printlist()
+llist.sort_list()
+print('The sorted list is: ')
+llist.printlist()
