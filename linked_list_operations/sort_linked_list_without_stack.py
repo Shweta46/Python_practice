4097328a692ba333d618367290c5dc56710934cf
shweta <aryashweta46@gmail.com>
2023-08-03 17:11:08 +0530

    quick sort and sort ll practice

--- linked_list_operations/sort_linked_list_without_stack.py
+++ linked_list_operations/sort_linked_list_without_stack.py
@@ -1,11 +1,46 @@
-string = 'GeeksforGeeks'
-freq = {}
-for i in string:
-    if i in freq:
-        freq[i] += 1
-    else:
-        freq[i] = 1
-print(freq)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-p = sorted(freq.values())
-print(p)
+class linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def sort_without_sort_function(self):
+        current = self.head
+        index = None
+        if current is None:
+            return
+        else:
+            while current is not None:
+                index = current.next
+                while index is not None:
+                    if current.data > index.data:
+                        current.data, index.data = index.data, current.data
+                    index = index.next
+                current = current.next
+    def printlist(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+llist = linkedlist()
+llist.push(11)
+llist.push(1)
+llist.push(10)
+llist.push(9)
+llist.push(7)
+llist.push(0)
+llist.push(2)
+llist.printlist()
+print('Unsorted list: ')
+llist.sort_without_sort_function()
+print('The sorted list is: ')
+llist.printlist()
--- sorting_algorithms/quick_sort.py
+++ sorting_algorithms/quick_sort.py
@@ -1,17 +1,18 @@
-def partition(array, low, high):
+def partition_function(array, low, high):
     pivot = array[high]
     i = low - 1
     for j in range(low, high):
         if array[j] <= pivot:
             i = i + 1
-            (array[i], array[j]) = (array[j], array[i])
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-    return i + 1
-def quickSort(array, low, high):
+            array[j], array[i] = array[i], array[j]
+    array[i+1], array[high] = array[high], array[i+1]
+    return i+1
+
+def quick_sort(array, low, high):
     if low < high:
-        pi = partition(array, low, high)
-        quickSort(array, low, pi - 1)
-        quickSort(array, pi + 1, high)
+        pi = partition_function(array, low, high)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
 
 data = [1, 7, 4, 1, 10, 9, -2]
 print("Unsorted Array")
@@ -19,7 +20,15 @@ print(data)
 
 size = len(data)
 
-quickSort(data, 0, size - 1)
+quick_sort(data, 0, size - 1)
 
 print('Sorted Array in Ascending Order:')
 print(data)
+
+
+
+
+
+
+
+
